2295defcb7cf10a58565cdcd41bc03fe4273f6d8
katherinelazar <catzar5@gmail.com>
2021-03-01 18:28:59 -0500

    updated settings.py adding two missed installed apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'marina_app',
 ]
 
