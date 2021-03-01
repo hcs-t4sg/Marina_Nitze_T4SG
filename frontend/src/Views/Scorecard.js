5671738f4e45b11f4a57ae03779dcfb2a3257536
katherinelazar <catzar5@gmail.com>
2021-03-01 18:21:34 -0500

    start making scorecard

--- frontend/src/Views/Scorecard.js
+++ frontend/src/Views/Scorecard.js
@@ -1,9 +1,38 @@
-// JavaScript source code
+import React, { Component } from "react";
+import * as d3 from "d3";
+
+class Scorecard extends Component {
+      constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+      }
+      componentDidUpdate() {
+          var color = d3.scaleOrdinal(d3.schemeBlues[9]);
+
+          d3.selectAll("svg").remove()
+
+          let svg = d3.select(this.myRef.current)
+             .append('svg')
+             .attr("class", "svg")
+             .attr('width', 400)
+             .attr('height', 400)
+             .style("margin-left", 100);
+
+          d3.json('http://localhost:8000/api/todos/')
+          .then(function(data) {
+            console.log(data);
+
+
+          });
+
+      }
+}
 
 export default function Scorecard() {
     return (
         <div>
             <h1> Scorecard </h1>
+            <div>class="scorecard"</div>
         </div>
 
     );
