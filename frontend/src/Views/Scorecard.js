74b239d943216e3a9da9d11939fafa7d6a616d00
katherinelazar <catzar5@gmail.com>
2021-03-01 19:27:09 -0500

    Initial scorecard: bringing some backend states data to the frontend

--- frontend/src/Views/Scorecard.js
+++ frontend/src/Views/Scorecard.js
@@ -1,37 +1,38 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 
-class Scorecard extends Component {
-      constructor(props) {
-        super(props);
-        this.myRef = React.createRef();
-      }
-      componentDidUpdate() {
-
-          d3.selectAll("svg").remove()
-
-          let svg = d3.select(this.myRef.current)
-             .append('svg')
-             .attr("class", "svg")
-             .attr('width', 400)
-             .attr('height', 400)
-             .style("margin-left", 100);
-
-          d3.json('http://localhost:8000/api/states/')
-          .then(function(data) {
-            console.log(data);
-
-
-          });
-
-      }
-}
 
 export default function Scorecard() {
+    var margin = {top: 40, right: 60, bottom: 60, left: 60};
+    var width = 1000 - margin.left - margin.right;
+    var height = 800 - margin.top - margin.bottom;
+
+    // SVG scorecard drawing area
+    var svg = d3.select(".scorecard").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.json('http://localhost:8000/api/states/')
+		.then(function(data) {
+		    console.log(data);
+
+		data.forEach( function(value, index) {
+			svg.append("text")
+		        .attr("x", 100)
+		        .attr("y", 100 * index)
+		        .text(value.name + " " + value.population);
+
+        });
+
+	});
+
+
     return (
         <div>
             <h1> Scorecard </h1>
-            <div>class="scorecard"</div>
+            <div class="scorecard"></div>
         </div>
 
     );
