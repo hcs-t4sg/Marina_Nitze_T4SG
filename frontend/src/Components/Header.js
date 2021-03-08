ca789bdbb319b3496e0b56342060a9136ac0e6e6
katherinelazar <catzar5@gmail.com>
2021-03-07 21:07:32 -0500

    Formatting header and menu button

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,9 +1,13 @@
 // JavaScript source code
 import "./Header.css"
 export default function Header(props) {
+
 	return (
-		<div className="header">
-			<button className="header-btn" onClick={props.onClick}> Toggle </button>
+		<div>
+			<div className="header">
+				<a className="header-btn" onClick={props.onClick}><span></span></a>
+			</div>
+			<div className="header-teal-section"></div>
 		</div>
 
 	)
