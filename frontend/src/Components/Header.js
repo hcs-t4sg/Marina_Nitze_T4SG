77f7dfbb5ebe221af694881a708e887af08a56a9
katherinelazar <catzar5@gmail.com>
2021-03-09 23:55:42 -0500

    thanks justin- now removing comments

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,45 +1,6 @@
 // JavaScript source code
 import "./Header.css"
 export default function Header(props) {
-	// var toggle = document.createElement('a', { is : 'toggle' });
-	// var toggle = document.createElement('a');
-	// toggle.innerHTML = '<span></span>';
-	// toggle.className = 'toggle';
-	// toggle.onClick = props.onClick;
-
-	//function hasClass(elem, className) {
-	//  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-	//}
-	//function addClass(elem, className) {
-	//    if (!hasClass(elem, className)) {
-	//      elem.className += ' ' + className;
-	//    }
-	//}
-	//function removeClass(elem, className) {
-	//  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-	//  if (hasClass(elem, className)) {
-	//        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-	//            newClass = newClass.replace(' ' + className + ' ', ' ');
-	//        }
-	//        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-	//    }
-	//}
-	//function toggleClass(elem, className) {
-	//  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-	//    if (hasClass(elem, className)) {
-	//        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-	//            newClass = newClass.replace( " " + className + " " , " " );
-	//        }
-	//        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-	//    } else {
-	//        elem.className += ' ' + className;
-	//    }
-	//}
-
-	//toggle.onclick = function() {
-	//   toggleClass(this, 'on');
-	//   return false;
-	//}
 
 	return (
 		<div>
