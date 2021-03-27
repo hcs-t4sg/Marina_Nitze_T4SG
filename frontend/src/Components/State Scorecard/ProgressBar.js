8028e7c3bfd7962fb48baa4e33286dc2d9a498f2
Someone-1243 <justinhye@gmail.com>
2021-03-27 19:50:21 -0400

    progress bar source

--- frontend/src/Components/State Scorecard/ProgressBar.js
+++ frontend/src/Components/State Scorecard/ProgressBar.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-
+// https://dev.to/ramonak/react-how-to-create-a-custom-progress-bar-component-in-5-minutes-2lcl
 export default function ProgressBar(props) {
     const completed = props.completed;
     const total = props.total;
