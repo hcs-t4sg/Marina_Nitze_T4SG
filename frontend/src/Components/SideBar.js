988a777b5f1c0483f61ca656ed33101749547bc5
katherinelazar <catzar5@gmail.com>
2021-03-29 12:13:53 -0400

    Removing map from side menu

--- frontend/src/Components/SideBar.js
+++ frontend/src/Components/SideBar.js
@@ -4,7 +4,6 @@ import "./SideBar.css";
 import home from "../res/house.png";
 import faq from "../res/faq.png";
 import bulb from "../res/lightbulb.png";
-import map from "../res/map.png";
 import graph from "../res/scorecard.png";
 import ribbon from "../res/ribbon.png";
 import { Link } from 'react-router-dom';
@@ -31,15 +30,6 @@ export default function SideBar(props) {
                     Scorecard
                 </Link>
 
-            </div>
-
-            <div className="nav-block">
-                <img className="side-image" src={map} />
-                <Link className="nav-link"
-                    to={{ pathname: '/map' }}
-                    onClick={props.click}>
-                    Map
-                </Link>
             </div>
 
             <div className="nav-block">
