fd719b7f291a68b7bd055bb26d1b03e0e03c14ab
katherinelazar <catzar5@gmail.com>
2021-04-05 13:45:35 -0400

    Added contacts header

--- frontend/src/Views/Contacts.js
+++ frontend/src/Views/Contacts.js
@@ -1,21 +1,22 @@
 // JavaScript source code
 import "../App.css"
+import "../Components/Contacts.css"
 import Subheader from "../Components/Subheader";
 
-export default function Resources() {
+export default function Contacts() {
     return (
         <div className="contacts-page">
 	        <div className="teal-section-container">
 	            <div className="teal-section"></div>
 	            <h2 className="header-subtitle"> Child Welfare Playbook </h2>
-	            <h1 className="title">About Us </h1>
+	            <h1 className="title">Contacts </h1>
 	        </div>
 
+	        <Subheader title="Contacts"/>
+
 	        <div className="contact-grid">
-                    <Subheader title="Contacts"/>
-                    <div className="text-row">
-                    </div>
-                </div>
+                <div className="text-row"></div>
+            </div>
 
         </div>
 
