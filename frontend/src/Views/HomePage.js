04363acbbdb53485cbf3421c5b1753ea700b190f
katherinelazar <catzar5@gmail.com>
2021-04-07 13:32:04 -0400

    Finished styling splash page. Prob need to fix button shadow, it looks weird

--- frontend/src/Views/HomePage.js
+++ frontend/src/Views/HomePage.js
@@ -5,13 +5,15 @@ import "../Components/HomePage.css";
 export default function HomePage() {
     return (
         <div className="homepage">
-        	<h2 className="header-subtitle"> Relevant data. Actionable solutions. </h2>
-            <h1 className="title"> Child Welfare Reform. </h1>
-            <h4 className="" > Description of the site and a high-level overview of its features. 
-            					Mention types of data provided, issue areas covered, and that there is 
-            					access to best practices.
-            					</h4>
-            <button type="button">Go!</button>
+        	<div className="content-container">
+	        	<div className="home-background"></div>
+	        	<h2 className="header-subtitle"> Relevant data. Actionable solutions. </h2>
+	            <h1 className="title"> Child Welfare Reform. </h1>
+	            <h4 className="description"> Description of the site and a high-level overview of its features. 
+	            					Mention types of data provided, issue areas covered, and that there is 
+	            					access to best practices. </h4>
+	            <button type="button" className="go-button">Go!</button>
+            </div>
         </div>
 
     );
