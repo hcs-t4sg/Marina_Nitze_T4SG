352e87164a365c6c2c106319af1366ac24b21686
annemfoley <31217196+annemfoley@users.noreply.github.com>
2021-04-12 11:24:29 -0400

    added subpractices

--- backend/marina_app/admin.py
+++ backend/marina_app/admin.py
@@ -3,20 +3,20 @@ from .models import IssueArea, State, Implementation, Contact
 
 class IssueAreaAdmin(admin.ModelAdmin):
     list_display = ('title', 'num_practices','intro_text','conclusion_text',
-                'practice_1','practice_1_question','practice_1_description','practice_1_quote','practice_1_link',
-                'practice_2','practice_2_question','practice_2_description','practice_2_quote','practice_2_link',
-                'practice_3','practice_3_question','practice_3_description','practice_3_quote','practice_3_link',
-                'practice_4','practice_4_question','practice_4_description','practice_4_quote','practice_4_link',
-                'practice_5','practice_5_question','practice_5_description','practice_5_quote','practice_5_link',
-                'practice_6','practice_6_question','practice_6_description','practice_6_quote','practice_6_link',
-                'practice_7','practice_7_question','practice_7_description','practice_7_quote','practice_7_link')
+                'practice_1','practice_1_question','practice_1_description','practice_1_quote','practice_1_link','subpractices_1_names',
+                'practice_2','practice_2_question','practice_2_description','practice_2_quote','practice_2_link','subpractices_2_names',
+                'practice_3','practice_3_question','practice_3_description','practice_3_quote','practice_3_link','subpractices_3_names',
+                'practice_4','practice_4_question','practice_4_description','practice_4_quote','practice_4_link','subpractices_4_names',
+                'practice_5','practice_5_question','practice_5_description','practice_5_quote','practice_5_link','subpractices_5_names',
+                'practice_6','practice_6_question','practice_6_description','practice_6_quote','practice_6_link','subpractices_6_names',
+                'practice_7','practice_7_question','practice_7_description','practice_7_quote','practice_7_link','subpractices_7_names')
 
 class StateAdmin(admin.ModelAdmin):
     list_display = ('name', 'abbreviation', 'population','county_administered')
 
 
 class ImplementationAdmin(admin.ModelAdmin):
-    list_display = ('state','issue_area','practice_1','practice_2','practice_3','practice_4',
+    list_display = ('state','issue_area','practice_1','subpractices_1','practice_2','subpractices_2','practice_3','practice_4',
                 'practice_5','practice_6','practice_7')
 
 class ContactAdmin(admin.ModelAdmin):
--- backend/marina_app/models.py
+++ backend/marina_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_mysql.models import ListCharField
 
 # Create your models here.
 
@@ -28,42 +29,50 @@ class IssueArea(models.Model):
     practice_1_description = models.TextField(default="", null=True, blank=True)
     practice_1_quote = models.TextField(default="", null=True, blank=True)
     practice_1_link = models.CharField(max_length=1000, default="", null=True, blank=True)
+    subpractices_1_names = ListCharField(base_field =  models.CharField(max_length=200), size=3, max_length = 602, null = True, blank = True)
 
     practice_2 = models.CharField(max_length=200, default="", null=True, blank=True)
     practice_2_question = models.TextField(default="", null=True, blank=True)
     practice_2_description = models.TextField(default="", null=True, blank=True)
     practice_2_quote = models.TextField(default="", null=True, blank=True)
     practice_2_link = models.CharField(max_length=1000, default="", null=True, blank=True)
+    subpractices_2_names = ListCharField(base_field = models.CharField(max_length=200), size=3, max_length = 602, null = True, blank = True)
+
 
     practice_3 = models.CharField(max_length=200, default="", null=True, blank=True)
     practice_3_question = models.TextField(default="", null=True, blank=True)
     practice_3_description = models.TextField(default="", null=True, blank=True)
     practice_3_quote = models.TextField(default="", null=True, blank=True)
     practice_3_link = models.CharField(max_length=1000, default="", null=True, blank=True)
+    subpractices_3_names = ListCharField(base_field =  models.CharField(max_length=200), size=3, max_length = 602, null = True, blank = True)
 
     practice_4 = models.CharField(max_length=200, default="", null=True, blank=True)
     practice_4_question = models.TextField(default="", null=True, blank=True)
     practice_4_description = models.TextField(default="", null=True, blank=True)
     practice_4_quote = models.TextField(default="", null=True, blank=True)
     practice_4_link = models.CharField(max_length=1000, default="", null=True, blank=True)
+    subpractices_4_names = ListCharField(base_field =  models.CharField(max_length=200), size=3, max_length = 602, null = True, blank = True)
 
     practice_5 = models.CharField(max_length=200, default="", null=True, blank=True)
     practice_5_question = models.TextField(default="", null=True, blank=True)
     practice_5_description = models.TextField(default="", null=True, blank=True)
     practice_5_quote = models.TextField(default="", null=True, blank=True)
     practice_5_link = models.CharField(max_length=1000, default="", null=True, blank=True)
+    subpractices_5_names = ListCharField(base_field =  models.CharField(max_length=200), size=3, max_length = 602, null = True, blank = True)
 
     practice_6 = models.CharField(max_length=200, default="", null=True, blank=True)
     practice_6_question = models.TextField(default="", null=True, blank=True)
     practice_6_description = models.TextField(default="", null=True, blank=True)
     practice_6_quote = models.TextField(default="", null=True, blank=True)
     practice_6_link = models.CharField(max_length=1000, default="", null=True, blank=True)
+    subpractices_6_names = ListCharField(base_field =  models.CharField(max_length=200), size=3, max_length = 602, null = True, blank = True)
 
     practice_7 = models.CharField(max_length=200, default="", null=True, blank=True)
     practice_7_question = models.TextField(default="", null=True, blank=True)
     practice_7_description = models.TextField(default="", null=True, blank=True)
     practice_7_quote = models.TextField(default="", null=True, blank=True)
     practice_7_link = models.CharField(max_length=1000, default="", null=True, blank=True)
+    subpractices_7_names = ListCharField(base_field =  models.CharField(max_length=200), size=3, max_length = 602, null = True, blank = True)
 
 
     def __str__(self):
@@ -74,12 +83,14 @@ class Implementation(models.Model):
     state = models.ForeignKey(State, on_delete=models.CASCADE)
     issue_area = models.ForeignKey(IssueArea, on_delete=models.CASCADE)
 
-    ImplementationStatuses = [('1','Implemented'),
-                              ('0','Not Implemented'),
-                              ('2', 'In Progress'),
-                              ('-1', 'No Data')]
+    ImplementationStatuses = [('Implemented','Implemented'),
+                              ('Not Implemented','Not Implemented'),
+                              ('In Progress', 'In Progress'),
+                              ('No Data', 'No Data')]
     practice_1 = models.CharField(max_length=20,  choices = ImplementationStatuses)
+    subpractices_1 = ListCharField(base_field = models.CharField(max_length=20,  choices = ImplementationStatuses), size=3, max_length = 63, null = True, blank = True)
     practice_2 = models.CharField(max_length=20, choices = ImplementationStatuses)
+    subpractices_2 = ListCharField(base_field = models.CharField(max_length=20,  choices = ImplementationStatuses), size=3, max_length = 63, null = True, blank = True)
     practice_3 = models.CharField(max_length=20, choices = ImplementationStatuses)
     practice_4 = models.CharField(max_length=20, choices = ImplementationStatuses)
     practice_5 = models.CharField(max_length=20, choices = ImplementationStatuses)
